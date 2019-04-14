a01dea9e88458693b62263901621dcd88c32afca
Jordi <jaarques@uoc.edu>
2019-04-15 00:09:02 +0200

    Updates

---
+++ precache-manifest.76d616986d97720c0abc91596a637786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f86f664c4e474c48b05",
+    "url": "/jaarques-uoc/boardgames-online-shop.github.io.git/static/css/main.e9f58245.chunk.css"
+  },
+  {
+    "revision": "3f86f664c4e474c48b05",
+    "url": "/jaarques-uoc/boardgames-online-shop.github.io.git/static/js/main.54c3b532.chunk.js"
+  },
+  {
+    "revision": "384c900ef62bcf22e81e",
+    "url": "/jaarques-uoc/boardgames-online-shop.github.io.git/static/js/runtime~main.b9afe516.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/jaarques-uoc/boardgames-online-shop.github.io.git/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jaarques-uoc/boardgames-online-shop.github.io.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5a4b900fdee99d1ebe9293008b39725",
+    "url": "/jaarques-uoc/boardgames-online-shop.github.io.git/index.html"
+  }
+];
