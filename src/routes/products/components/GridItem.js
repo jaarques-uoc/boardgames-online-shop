535c5b2a091978ffbf88a7f36688b1ea847074c4
Jordi <jaarques@uoc.edu>
2019-04-15 20:51:21 +0200

    Formatting grid items

--- src/routes/products/components/GridItem.js
+++ src/routes/products/components/GridItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const GridItem = ({}) =>
-    <React.Fragment>
+    <div className="grid-item">
         <img src="https://static.cardmarket.com/img/548dd39417c935651fbd98c3ee6d5951/items/1/WAR/372131.jpg"/>
         <div>Item Name</div>
-    </React.Fragment>;
+        <div>3 €</div>
+        <button type="button" className="btn btn-primary">Add to cart</button>
+    </div>;
 
 export {GridItem};
--- src/routes/products/components/GridRow.js
+++ src/routes/products/components/GridRow.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {GridItem} from "./GridItem";
 
 const GridRow = ({}) =>
-    <React.Fragment>
-        <div className="row">
-            <div className="col-6 col-md-4 text-center"><GridItem/></div>
-            <div className="col-6 col-md-4 text-center"><GridItem/></div>
-            <div className="col-6 col-md-4 text-center"><GridItem/></div>
-        </div>
-    </React.Fragment>;
+    <div className="row grid-row">
+        <div className="col-6 col-md-2 text-center"><GridItem/></div>
+        <div className="col-6 col-md-2 text-center"><GridItem/></div>
+        <div className="col-6 col-md-2 text-center"><GridItem/></div>
+        <div className="col-6 col-md-2 text-center"><GridItem/></div>
+        <div className="col-6 col-md-2 text-center"><GridItem/></div>
+        <div className="col-6 col-md-2 text-center"><GridItem/></div>
+    </div>;
 
 export {GridRow};
