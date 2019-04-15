8e29a31d1a08a482048c599b6ce20bc9a0bc3449
Jordi <jaarques@uoc.edu>
2019-04-15 19:54:02 +0200

    Creating grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import {Navbar} from "./common/components/Navbar";
+import {Grid} from "./common/components/Grid";
 
 class App extends Component {
     render() {
         return (
             <React.Fragment>
                 <Navbar/>
+                <Grid/>
             </React.Fragment>
         );
     }
---
+++ src/common/components/Grid.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Grid = ({}) =>
+    <React.Fragment>
+        <div className="row">
+            <div className="col-6 col-md-4 text-center">.col-6 .col-md-4</div>
+            <div className="col-6 col-md-4 text-center">.col-6 .col-md-4</div>
+            <div className="col-6 col-md-4 text-center">.col-6 .col-md-4</div>
+        </div>
+        <div className="row">
+            <div className="col-6 col-md-4 text-center">.col-6 .col-md-4</div>
+            <div className="col-6 col-md-4 text-center">.col-6 .col-md-4</div>
+            <div className="col-6 col-md-4 text-center">.col-6 .col-md-4</div>
+        </div>
+    </React.Fragment>;
+
+export {Grid};
