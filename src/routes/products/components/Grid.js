8d076f922b3fe1d646462e35c9b22d107e92ce9f
Jordi <jaarques@uoc.edu>
2019-04-17 22:51:33 +0200

    Dynamically position the elements on the grid

--- src/routes/products/components/Grid.js
+++ src/routes/products/components/Grid.js
@@ -2,9 +2,35 @@ import React from 'react';
 import {GridRow} from "./GridRow";
 import {fetchProducts} from "../productsDAO";
 
-const Grid = ({}) =>
-    <React.Fragment>
-        <GridRow productsRow={fetchProducts()}/>
-    </React.Fragment>;
+const toMatrix = (array, numCols) => {
+    const addElementToCell = (array, x, i) => {
+        const rowNumber = Math.floor(i / numCols);
+
+        if (i % numCols === 0) {
+            array[rowNumber] = [];
+        }
+
+        array[rowNumber].push(x);
+    };
+
+    const matrix = [];
+
+    array.map((x, i) => addElementToCell(matrix, x, i));
+
+    return matrix;
+
+};
+
+const Grid = ({}) => {
+    const grid = toMatrix(fetchProducts(), 6);
+
+    return (
+        <React.Fragment>
+            {
+                grid.map(gridRow => <GridRow productsRow={gridRow}/>)
+            }
+        </React.Fragment>
+    );
+};
 
 export {Grid};
