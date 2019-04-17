1621034b1dabea1bb5c1112643615dba753249ca
Jordi <jaarques@uoc.edu>
2019-04-18 00:41:05 +0200

    Fix product mock

--- src/routes/products/productsDAO.js
+++ src/routes/products/productsDAO.js
@@ -2,7 +2,7 @@ const fetchProducts = () => [...Array(20).keys()].map(buildRandomProduct);
 
 const getProduct = productId => buildRandomProduct(productId);
 
-const buildRandomProduct = position => buildProduct(position, `Item name ${position}`, 'Some description 1', position + 0.34, 'https://static.cardmarket.com/img/548dd39417c935651fbd98c3ee6d5951/items/1/WAR/372131.jpg');
+const buildRandomProduct = position => buildProduct(position, `Item name ${position}`, 'Some description 1', parseInt(position) + 0.34, 'https://static.cardmarket.com/img/548dd39417c935651fbd98c3ee6d5951/items/1/WAR/372131.jpg');
 
 const buildProduct = (id, name, description, price, imageUrl) => ({
     id,
