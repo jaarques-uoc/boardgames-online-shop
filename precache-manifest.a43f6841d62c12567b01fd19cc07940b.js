5b604612e00b3082d5a8f20fa5db63f5a99ef336
Jordi <jaarques@uoc.edu>
2019-04-28 12:17:29 +0200

    Updates

---
+++ precache-manifest.a43f6841d62c12567b01fd19cc07940b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0f0267420052575f690",
+    "url": "/static/css/main.9bbc8920.chunk.css"
+  },
+  {
+    "revision": "c0f0267420052575f690",
+    "url": "/static/js/main.40397ca6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1acc40b17fab911c9a18",
+    "url": "/static/css/2.fe2aa136.chunk.css"
+  },
+  {
+    "revision": "1acc40b17fab911c9a18",
+    "url": "/static/js/2.82006058.chunk.js"
+  },
+  {
+    "revision": "6713eb1827e01524245722e1f74115fb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4035b54c32dea8f73874bf39083ad28.js"
+  "/precache-manifest.a43f6841d62c12567b01fd19cc07940b.js"
 );
 
 workbox.clientsClaim();
