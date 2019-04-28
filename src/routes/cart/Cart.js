71b707098277b532733cc3e8d31df9543e09f055
Jordi <jaarques@uoc.edu>
2019-04-28 12:17:01 +0200

    Adding dropdown to choose number of items to buy in cart page

--- src/routes/cart/Cart.js
+++ src/routes/cart/Cart.js
@@ -2,40 +2,62 @@ import React from 'react';
 import {getCart} from "./cartDAO";
 import {Link} from "react-router-dom";
 
-const Cart = () => {
-    const cart = getCart();
-    const totalPrice = cart.products.reduce((acc, item) => acc + item.quantity * item.product.price, 0);
+class Cart extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="container content-padding">
-            <div className="row justify-content-center">
-                <div className="col-8">
-                    <table className="table">
-                        <thead className="thead-light text-center">
-                        <tr>
-                            <th scope="col">Product</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Total price</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {cart.products.map((cartItem, key) => <CartItem cartItem={cartItem} key={key}/>)}
-                        <tr>
-                            <td colSpan="5" className="text-right">
-                                {totalPrice} €
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+        this.state = {
+            cart: getCart(),
+            loading: false
+        }
+    }
+
+    calculateTotalPrice = () => this.state.cart.products
+        .reduce((acc, item) => acc + item.quantity * item.product.price, 0);
+
+    onChange = item => event => {
+        this.setState({loading: true});
+        console.log(item);
+        console.log(event.target.value);
+    };
+
+    render() {
+        return (
+            <div className="container content-padding">
+                <div className="row justify-content-center">
+                    <div className="col-8">
+                        <table className="table">
+                            <thead className="thead-light text-center">
+                            <tr>
+                                <th scope="col">Product</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Total price</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.cart.products.map((cartItem, key) =>
+                                <CartItem cartItem={cartItem}
+                                          key={key}
+                                          onChange={this.onChange(cartItem.product)}
+                                          disabled={this.state.loading}/>
+                            )}
+                            <tr>
+                                <td colSpan="5" className="text-right">
+                                    {this.calculateTotalPrice()} €
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
-const CartItem = ({cartItem}) => {
+const CartItem = ({cartItem, onChange, disabled}) => {
     const totalItemPrice = (cartItem.product.price * cartItem.quantity).toFixed(2);
 
     return (
@@ -44,21 +66,19 @@ const CartItem = ({cartItem}) => {
             <td className="align-middle">{cartItem.product.name}</td>
             <td className="align-middle text-right">{cartItem.product.price} €</td>
             <td className="align-middle">
-                <div className="dropdown">
-                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div>
+                <QuantityDropdown quantity={cartItem.quantity}
+                                  onChange={onChange}
+                                  disabled={disabled}/>
             </td>
             <td className="align-middle text-right">{totalItemPrice} €</td>
         </tr>
     );
 };
+
+const QuantityDropdown = ({quantity, onChange, disabled}) =>
+    <select onChange={onChange} disabled={disabled}>
+        {[...Array(11).keys()]
+            .map(x => <option value={x} key={x} selected={quantity === x}>{x}</option>)}
+    </select>;
 
 export {Cart};
