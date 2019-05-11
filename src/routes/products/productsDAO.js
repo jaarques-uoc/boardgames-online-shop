2a0c0fe6e3fb7c2a9b3ef6fe3332dcf153c74359
Jordi <jaarques@uoc.edu>
2019-05-11 20:07:37 +0200

    Fixing url

--- src/routes/products/productsDAO.js
+++ src/routes/products/productsDAO.js
@@ -1,10 +1,10 @@
 import {URLS} from "../../common/urls";
 import {doGet} from "../../common/helperDAO";
 
-const fetchProducts = () => doGet(`${URLS.PRODUCTS_WS}/customers/`)
+const fetchProducts = () => doGet(`${URLS.PRODUCTS_WS}/products/`)
     .catch(e => console.log(`Error when retrieving productss: ${e}`));
 
-const getProduct = id => doGet(`${URLS.PRODUCTS_WS}/customers/${id}`)
+const getProduct = id => doGet(`${URLS.PRODUCTS_WS}/products/${id}`)
     .catch(e => console.log(`Error when retrieving product ${id}: ${e}`));
 
 export {fetchProducts, getProduct};
